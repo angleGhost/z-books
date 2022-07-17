25713fe2265038e849dd06eb0ea50491ecce2ac3
TBH <495140477@qq.com>
2022-07-17 18:57:21 +0800

    文件上传基础实现

--- src/test/java/com/zbooks/JasyptTest.java
+++ src/test/java/com/zbooks/JasyptTest.java
@@ -1,10 +1,8 @@
 package com.zbooks;
 
 import org.jasypt.encryption.StringEncryptor;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -12,25 +10,13 @@ public class JasyptTest {
     @Autowired
     private StringEncryptor encryptor;
 
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.url}")
-    private String url;
-
     @Test
-    @Ignore
     public void contextLoads() {
-        System.out.println("MySQL 用户名：" + encryptor.encrypt(username));
-        System.out.println("MySQL 密码：" + encryptor.encrypt(password));
-        System.out.println("MySQL URl：" + encryptor.encrypt(url));
+        System.out.println("====>" + encryptor.encrypt(""));
     }
 
     @Test
     public void decryptString() {
-        System.out.println("MySQL 用户名：" + encryptor.decrypt("message"));
+        System.out.println("====>" + encryptor.decrypt(""));
     }
 }
